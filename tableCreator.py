7c5af979186c735a743a92d62bbcb0c74f5ae4e9
Roy O. Jacome <rjacomejr96@aol.com>
2018-10-18 18:18:45 -0400

    Updated tableCreator. Can make lists of given values for attriubutes. Now needs to be randomized.

--- tableCreator.py
+++ tableCreator.py
@@ -6,7 +6,60 @@ def createTable(tableName, attributes):
     # Get all lists needed to populate the table (how?)
     # Create .csv file with attributes at top
     # Populate the columns with corresponding names/elements
-    pass
+
+    listOfTuples = list(range(len(attributes)))
+    maxAttributes = len(attributes)
+    attributeIndex = 0
+
+    # Create list of lists of all values according to column attributes
+    while attributeIndex < maxAttributes:
+        
+        # Take in files for each column
+        inputMessage = "Enter file name of values for attribute: '{}'\n".format(attributes[attributeIndex])
+        fileToExtract = input(inputMessage) 
+        listToAppend = []
+
+        # Ask how many values we should extract (Can be omitted)
+        while True:
+            try:
+                limit = int(input("Enter how many values to enter: "), 10)
+                if(limit <= 0):
+                    print("Please enter a positive integer")
+                    continue
+                print(limit)
+                break
+            except KeyboardInterrupt as exception:
+                print(exception)
+                break
+            except ValueError as exception:
+                print("Please enter a valid integer")
+                print(exception)
+                continue
+        
+        # Attempt to open file and extract contexts
+        try:
+            with open(fileToExtract, "r") as valuesFile:
+                limitCount = 0
+                for line in valuesFile:
+                    limitCount += 1
+                    listToAppend.append(line.rstrip())
+                    if(limitCount == limit):
+                        break
+        except FileNotFoundError as exception:
+            print("File does not exist!")
+            print("Try another")
+            print(exception)
+            continue
+
+        print(listToAppend)
+        print()
+        print()
+        print()
+        listOfTuples[attributeIndex] = listToAppend
+        print(listOfTuples)
+        attributeIndex += 1
+
+            
 
 
 def main():
@@ -20,12 +73,25 @@ def main():
     print("-----Input attributes of file-----")
     print("Type 'DONE' when finished")
     print("Type 'RESET' to start over")
+    print("Type '-rm <attribute>' to remove an attribute")
     print("Type 'EXIT' to kill program")
     print()
 
     while True:
 
         inputAttribute = input("Input Attribute: ")
+
+        # Check if we have more than one string
+        if(len(inputAttribute.split()) >= 2):
+            checkFlag = inputAttribute.split()[0]
+            
+            deleteAttribute = inputAttribute.split()[1]
+            if(checkFlag == "-rm"):
+                if(deleteAttribute in attributes):
+                    attributes.remove(deleteAttribute)
+                    print("Attribute removed!")
+                    print(attributes)
+                    continue
        
         # Done inputting attributes
         if(inputAttribute == "DONE"):
@@ -45,8 +111,15 @@ def main():
 
         print(attributes)
 
+    # Just so we dont crash later on (can be removed if we want to)
+    if attributes == []:
+        print("Empty list not valid")
+        exit()
+
     print(tableName)
     print(attributes)
+
+    createTable(tableName, attributes)
 
 if __name__ == "__main__":
     main()
