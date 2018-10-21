73f6c45133b6ae507dfa66c47600eb0727d4d208
Roy O. Jacome <rjacomejr96@aol.com>
2018-10-21 02:10:26 -0400

    Can now populate csv files with given attributes

--- tableCreator.py
+++ tableCreator.py
@@ -1,10 +1,9 @@
 import webScraper
 import sys
+import csv
 import random
 
 def getListOfValues(attributes):
-    #TODO
-    # Get all lists needed to populate the table (how?)
 
     listOfTuples = list(range(len(attributes)))
     maxAttributes = len(attributes)
@@ -57,17 +56,18 @@ def getListOfValues(attributes):
 
     return listOfTuples
 
-
+# Function that produces random tuples of indicated amount
 def randomizeTuples(listOfTuples):
 
     elementsCount = len(listOfTuples)
 
+    # Get input on how many random tuples to make
     while True:
         try:
             message = "Enter how many random tuples to make: "
             randomTuplesCount = int(input(message), 10)
-            if(randomTuplesCount <= 10):
-                print("At least more than 10 tuples")
+            if(randomTuplesCount <= 2):
+                print("At least more than 2 tuples")
                 continue
             else:
                 break
@@ -77,19 +77,26 @@ def randomizeTuples(listOfTuples):
 
     randomizedTuples = list(range(randomTuplesCount))
 
-    for tupleCount in range(randomTuplesCount):
+    tupleCount = 0
+
+    # Create random (distinct) tuples and insert them into final list
+    while tupleCount < randomTuplesCount:
         row = []
         for attributeCount in range(elementsCount):
             row.append(random.choice(listOfTuples[attributeCount]))
-        randomizedTuples[tupleCount] = row  
+        
+        if(row in randomizedTuples):
+            print("Duplicte random tuple...")
+            print(row)
+            print()
+        else:
+            randomizedTuples[tupleCount] = row  
+            tupleCount += 1
 
     return  randomizedTuples  
 
-
-
-def main():
-
-    tableName = sys.argv[1]
+# Get attributes from user
+def getAttributes():
 
     attributes = []
     inputAttribute = None
@@ -103,8 +110,10 @@ def main():
     print()
 
     while True:
-
-        inputAttribute = input("Input Attribute: ")
+        
+        message = "Input Attribute: "
+        inputAttribute = input(message).rstrip()
+        inputAttribute = inputAttribute.strip()
 
         # Check if we have more than one string
         if(len(inputAttribute.split()) >= 2):
@@ -116,6 +125,9 @@ def main():
                     attributes.remove(deleteAttribute)
                     print("Attribute removed!")
                     print(attributes)
+                    continue
+                else:
+                    print("Attribute not in list!")
                     continue
        
         # Done inputting attributes
@@ -136,10 +148,28 @@ def main():
 
         print(attributes)
 
-    # Just so we dont crash later on (can be removed if we want to)
-    if attributes == []:
-        print("Empty list not valid")
-        exit()
+        # Just so we dont crash later on (can be removed if we want to)
+        if attributes == []:
+            print("Empty list not valid")
+            exit()
+
+    return attributes
+
+def populateTable(tableName, attributes, randomizedTuplesList):
+    
+    with open(tableName, "w") as file:
+        csvfile = csv.writer(file, delimiter=',')
+        csvfile.writerow(attributes)
+        tupleRow = 0
+        for row in randomizedTuplesList:
+            csvfile.writerow(row)
+            tupleRow += 1
+
+def main():
+
+    tableName = sys.argv[1]
+
+    attributes = getAttributes()
 
     listOfTuples = getListOfValues(attributes)
 
@@ -147,6 +177,8 @@ def main():
     print(listOfTuples)
 
     randomizedTuplesList = randomizeTuples(listOfTuples)
+
+    populateTable(tableName, attributes, randomizedTuplesList)
 
     print(randomizedTuplesList)
 
