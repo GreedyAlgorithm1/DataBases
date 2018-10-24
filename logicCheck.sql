af47d16c9cb37b4a20d1666c4bdbd6efce517500
GreedyAlgorithm1 <sk1683@scarletmail.rutgers.edu>
2018-10-24 18:57:51 -0400

    Add files via upload
    Logic check to make sure input follows pattern

---
+++ logicCheck.sql
@@ -0,0 +1,37 @@
+/*Transactions/bills can't be issued when given bar is closed*/
+DROP FUNCTION IF EXISTS check_bill_time
+CREATE FUNCTION check_bill_time(timeOfIssue int, barID int) return varchar(50)
+BEGIN
+	if(
+		exists(
+			select b.bar
+			from bars b JOIN operationHours oH on b.barID = oH.barID
+			where b.barID = barID and timeOfIssue < b.CloseTime and timeOfIssue > b.Opentime 
+			)
+		)return 'True'
+	else
+	return 'False'
+END;
+
+/*drinkers can't frequent bars in different state*/
+/*NOTE THIS IS ONLY DONE ON NEW INPUTS*/
+DROP FUNCTION IF EXISTS check_state
+CREATE FUNCTION check_state(drinker_inpt varchar(50), barState varchar(8)) RETURNS VARCHAR(50)
+BEGIN
+	if ( 
+		EXISTS (
+			select state
+			from drinkers
+			where drinker_input = name and barState = state
+	) RETURN 'True'
+	else 
+	return 'False'
+END;
+ 
+/*beer heirarchy established*/
+/*
+DROP FUNCTION IF EXISTS check_beer_order
+CREATE FUNCTION check_beer_order(beer varchar) RETURNS VARCHAR
+BEGIN
+	if
+*/
